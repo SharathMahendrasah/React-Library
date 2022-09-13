00631b151d235b58ce8cb2fae1d0152781146e3c
Sharath Mahendrasah <sharath@mail.usf.edu>
2022-09-13 11:22:12 -0400

    Added Cart component. Now able to add a book and then once it is added go to cart or click on another book add it then go to cart.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    console.log(book, quantity);
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  function removeItem(item) {
+    setCart((oldCart) => oldCart.filter((cartItem) => cartItem.id !== item.id));
+  }
+
+  function changeQuantity() {}
+
   return (
     <Router>
       <div className="App">
@@ -14,7 +36,18 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/books" exact element={<Books books={books} />} />
-          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart books={books} cart={cart} changeQuantity={changeQuantity} />
+            }
+          />
         </Routes>
         <Footer />
       </div>
